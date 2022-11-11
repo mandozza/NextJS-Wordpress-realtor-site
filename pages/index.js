4625a614e64cba9ed0eefdb3a1b15b10b310e989
Jeremy Josey <webjeremy@gmail.com>
2022-11-11 08:45:10 -0400

    MainMenu query and cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import client from "client";
 import { gql } from "@apollo/client";
-import { cleanAndTransformBlocks } from 'utils/cleanAndTransformBlocks'
+import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks"
 import { BlockRenderer } from "components/BlockRenderer";
+import { mapMainMenuItems } from "utils/mapMainMenuItems"
 
 export default function Home(props) {
+  console.log(props)
   return <div>
     <BlockRenderer blocks={props.blocks}/>
   </div>;
@@ -12,7 +14,7 @@ export default function Home(props) {
 export const getStaticProps = async () => {
   const { data } = await client.query({
     query: gql`
-      query NewQuery {
+      query PageQuery {
         nodeByUri(uri: "/") {
           ... on Page {
             id
@@ -20,17 +22,42 @@ export const getStaticProps = async () => {
             blocksJSON
           }
         }
+        acfOptionsMainMenu {
+          mainMenu {
+            menuItems {
+              menuItem {
+                destination {
+                  ... on Page {
+                    uri
+                  }
+                }
+                label
+              }
+              items {
+                destination {
+                  ... on Page {
+                    uri
+                  }
+                }
+                label
+              }
+            }
+          }
+        }
       }
     `,
-  })
+  });
 
   // clean block data before sending to page.
   const blocks = cleanAndTransformBlocks(data.nodeByUri.blocksJSON);
 
   return {
     props: {
+      mainMenuItems: mapMainMenuItems(
+        data.acfOptionsMainMenu.mainMenu.menuItems
+      ),
       blocks,
-    }
-  }
+    },
+  };
 
 }
