e927bade1af4508d977c0cf34c4afc0ebb92d205
Jeremy Josey <webjeremy@gmail.com>
2022-11-12 13:34:40 -0400

    Added Columns and Groups

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,12 +1,18 @@
 import { CallToActionButton } from "components/CallToActionButton";
+import { Columns } from "components/Columns";
+import { Column } from "components/Column";
 import { Cover } from "components/Cover"
 import { Heading } from "components/Heading"
 import { Paragraph } from "components/Paragraph"
+import Image from "next/image"
 import { theme } from "theme"
 
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map(block => {
     switch (block.name) {
+      case "core/block": {
+        return <BlockRenderer key={block.id} blocks={block.innerBlocks} />;
+      }
       case "core/cover": {
         return (
           <Cover key={block.id} background={block.attributes.url}>
@@ -23,6 +29,37 @@ export const BlockRenderer = ({ blocks }) => {
             align={block.attributes.data.align}
           />
         );
+      }
+      case "core/columns": {
+        return (
+          <Columns
+            key={block.id}
+            isStackOnMobile={block.attributes.isStackOnMobile}
+          >
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Columns>
+        );
+      }
+      case "core/column": {
+        return (
+          <Column
+            key={block.id}
+            width={block.attributes.width}
+            textColor={
+              theme[block.attributes.textColor] ||
+              block.attributes.style?.color?.text
+            }
+            backgroundColor={
+              theme[block.attributes.backgroundColor] ||
+              block.attributes.style?.color?.background
+            }
+          >
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Column>
+        );
+      }
+      case "core/group": {
+        return <BlockRenderer key={block.id} blocks={block.innerBlocks} />;
       }
       case "core/heading": {
         return (
@@ -45,6 +82,17 @@ export const BlockRenderer = ({ blocks }) => {
               theme[block.attributes.textColor] ||
               block.attributes.style?.color?.text
             }
+          />
+        );
+      }
+      case "core/image": {
+        return (
+          <Image
+            key={block.id}
+            src={block.attributes.url}
+            height={block.attributes.originalHeight}
+            width={block.attributes.originalWidth}
+            alt={block.attributes.alt || ""}
           />
         );
       }
--- utils/cleanAndTransformBlocks.js
+++ utils/cleanAndTransformBlocks.js
@@ -3,8 +3,8 @@ import client from "client";
 import {v4 as uuid} from 'uuid'
 
 export const cleanAndTransformBlocks = async (blocksJSON) => {
-  const {data} = await client.query({
-    query: gql `
+  const { data } = await client.query({
+    query: gql`
       query ExtraDataQuery {
         pages {
           nodes {
@@ -12,9 +12,18 @@ export const cleanAndTransformBlocks = async (blocksJSON) => {
             databaseId
           }
         }
+        mediaItems(where: { offsetPagination: { size: 10000 } }) {
+          nodes {
+            databaseId
+            mediaDetails {
+              height
+              width
+            }
+          }
+        }
       }
-    `
-  })
+    `,
+  });
   const blocks = JSON.parse(blocksJSON)
   // Define keys to remove from block data that wont's be needed.
   const deleteKeys = [
@@ -34,10 +43,23 @@ export const cleanAndTransformBlocks = async (blocksJSON) => {
       deleteKeys.forEach((deleteKey) => {
         delete block[deleteKey];
       })
+      // check if its a cta button
       if(block.name === 'acf/ctabutton'){
         const associatedPage = data.pages.nodes.find((page) => page.databaseId === block.attributes.data.destination );
         if(associatedPage) {
           block.attributes.data.destination = associatedPage.uri;
+        }
+      }
+      // check if it's an image
+      if (block.name === "core/image") {
+        const associatedMediaItem = data.mediaItems.nodes.find(
+          (mediaItem) => mediaItem.databaseId === block.attributes.id
+        );
+        if (associatedMediaItem) {
+          block.attributes.originalHeight =
+            associatedMediaItem.mediaDetails.height;
+          block.attributes.originalWidth =
+            associatedMediaItem.mediaDetails.width;
         }
       }
 
