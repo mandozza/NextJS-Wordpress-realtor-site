bf2cd9cd0087ec800d0affedbf1d22cdf8f66284
Jeremy Josey <webjeremy@gmail.com>
2022-11-13 14:50:30 -0400

    Added PropertyCard

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,15 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Results } from "./Results";
 
 export const PropertySearch = () => {
+  const [properties, setProperties] = useState([])
+
   useEffect(() => {
     const search = async () => {
       const response = await fetch(`/api/search`)
       const data = await response.json()
       console.log(data)
+      setProperties(data.properties)
     }
     search();
 
   },[])
 
-  return <div>Property Search</div>;
+  return <div>
+    <Results properties={properties} />
+  </div>;
 };
